d85b02d2b73f71112e57b83dc429503418cecd5c
clemeuh <84191723+clemeuh@users.noreply.github.com>
2021-07-19 16:34:42 +0200

    sortie

--- src/Form/SortieType.php
+++ src/Form/SortieType.php
@@ -10,13 +10,16 @@ use App\Entity\Sortie;
 
 
 use Symfony\Component\Form\AbstractType;
+
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TimeType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -28,27 +31,46 @@ class SortieType extends AbstractType
             ->add('Nom', TextType::class)
 
             ->add('dateHeureDebut', DateTimeType::class,[
-                'attr'=>['class'=>'form-control'],
+                'attr'=>['class'=>'form-control js-datepicker'],
                 'label'=>'Date et Heure de sortie',
                 'required'=>true,
-                'widget'=>'single_text',
-                'html5'=>false,
+                'widget'=>'single_text'
 
             ])
 
+            ->add('nbInscriptionMax', null)
 
-            ->add('nbInscriptionMax', NumberType::class )
+            ->add('duree', ChoiceType::class, array(
+                'multiple' => true,
+                'expanded' => true,
+                'choices' => [
+                    '1h' => '1H00',
+                    '1h30' => '1H30',
+                    '2H' => '2H00',
+                    '2H30'=>'2H30',
+                    '3H00'=>'3H00',
+                    '3H30'=>'3H30',
+                    '4H00'=>'4H00',
+                    '4H30'=>'4H30',
+                    '5H00'=>'5H00',
+                    '5H30'=>'5H30',
+                    '6H00'=>'6H00',
+                    '6H30'=>'6H30',
 
-            ->add('duree', ChoiceType::class, [
-                'attr' => ['class'=>'data-list'],
-                'label'=>'Durée de la sortie',])
+            ]))
+
+
+            ->add('dateLimiteInscription', DateTimeType::class,[
+                'attr'=>['class'=>'form-control js-datepicker'],
+                'label'=>'Date de cloture',
+                'required'=>true,
+                'widget'=>'single_text'
+            ])
 
             ->add('infosSortie', TextareaType::class, ['required' => false])
 
-            ->add('save', SubmitType::class,[
-                'label' =>'Enregistrer',
-                'attr' => ['class' => 'btn btn-success w-100']
-            ])
+
+
             ->add('publish', SubmitType::class,[
                 'label' =>'Publier',
                 'attr' => ['class' => 'btn btn-primary w-100']
@@ -72,4 +94,3 @@ class SortieType extends AbstractType
     }
 
 }
-
