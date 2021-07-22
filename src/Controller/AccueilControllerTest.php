440465870d77f2c1c411c00b4ab921e5f4a19aca
KanToin <84191245+KanToin@users.noreply.github.com>
2021-07-22 14:53:53 +0200

    Modification acceuil test

--- src/Controller/AccueilControllerTest.php
+++ src/Controller/AccueilControllerTest.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 
+use App\Entity\Participant;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,7 +13,12 @@ class AccueilControllerTest extends AbstractController
     /**
      * @Route ("/accueiltest", name="accueiltest")
      */
-    public function accueil() {
-        return $this->render('accueil/accueiltest.html.twig');
+    public function accueil(EntityManagerInterface $em) {
+
+        $user = $em->getRepository(Participant::class);
+
+        return $this->render('accueil/accueiltest.html.twig', [
+            'user' => $user,
+        ]);
     }
 }
